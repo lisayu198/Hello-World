d0fbb2d66d2d2588d789528b9fba125f30bae0ba
lisayu198 <69992573+lisayu198@users.noreply.github.com>
2020-08-20 15:13:34 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Elizabeth Yu");
+		System.out.println("But I go by the nickname Lisa");
+		System.out.println("I like baking");
+		System.out.println("I like hiking");
+		System.out.println("I like to watch YouTube videos in my free time");
+	}
+}
